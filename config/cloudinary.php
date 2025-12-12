58005621d0099f945a3859d432639831c05e8872
DerrickWijaya <one.derrickwijaya@gmail.com>
2025-12-12 17:16:29 +0700

    Fix Cloudinary config structure

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -4,5 +4,7 @@ return [
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
     'api_key' => env('CLOUDINARY_API_KEY'),
     'api_secret' => env('CLOUDINARY_API_SECRET'),
+    'url' => env('CLOUDINARY_URL'),
     'secure' => true,
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
 ];
